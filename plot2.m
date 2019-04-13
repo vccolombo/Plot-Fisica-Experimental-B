f7ed786aac714cd2e96b7f3854edfc95673b324f
Víctor Colombo <victorcora@hotmail.com>
2019-04-13 00:35:27 -0300

    Add colors input to plot2 and plot2yy

--- plot2.m
+++ plot2.m
@@ -4,11 +4,19 @@
 # yl1 = Nome do eixo Y
 # leg1 = Legenda da curva Y1
 # leg2 = Legenda da curva Y2
-function plot2(X, Y1, Y2, X_err, Y1_err, Y2_err, tit, xl, yl, leg1, leg2)
+# color1 = Cor da curva Y1
+# color2 = Cor da curva Y2
+function plot2(X, Y1, Y2, 
+  X_err, Y1_err, Y2_err, 
+  tit, 
+  xl, yl, 
+  leg1, leg2, 
+  color1 = "b", color2 = "r")
+  
   hold on;
-  errorbar(X, Y1, X_err, X_err, Y1_err, Y1_err, "~>");
-  errorbar(X, Y2, X_err, X_err, Y2_err, Y2_err, "~>");
-
+  h1 = errorbar(X, Y1, X_err, X_err, Y1_err, Y1_err, "~>");
+  h2 = errorbar(X, Y2, X_err, X_err, Y2_err, Y2_err, "~>");
+  
   [a, b] = plotInterval(X);
 
   title(tit);
@@ -16,4 +24,6 @@ function plot2(X, Y1, Y2, X_err, Y1_err, Y2_err, tit, xl, yl, leg1, leg2)
   xlim([a, b]);
   ylabel(yl);
   legend(leg1, leg2);
+  set(h1, "color", color1);
+  set(h2, "color", color2);
 endfunction
--- plot2yy.m
+++ plot2yy.m
@@ -5,13 +5,21 @@
 # yl2 = Nome do eixo Y da direita
 # leg1 = Legenda da curva Y1
 # leg2 = Legenda da curva Y2
-function plot2yy(X, Y1, Y2, X_err, Y1_err, Y2_err, tit, xl, yl1, yl2, leg1, leg2)
+# color1 = Cor da curva Y1
+# color2 = Cor da curva Y2
+function plot2yy(X, Y1, Y2, 
+  X_err, Y1_err, Y2_err, 
+  tit, 
+  xl, yl1, yl2, 
+  leg1, leg2, 
+  color1 = "b", color2 = "r")
+  
   ax = plotyy(X, Y1, X, Y2);
   hold(ax(1), 'on');
-  errorbar(ax(1), X, Y1, X_err, X_err, Y1_err, Y1_err, "~>");
+  h1 = errorbar(ax(1), X, Y1, X_err, X_err, Y1_err, Y1_err, "~>");
   hold(ax(2), 'on');
-  errorbar(ax(2), X, Y2, X_err, X_err, Y2_err, Y2_err, "~>");
-
+  h2 = errorbar(ax(2), X, Y2, X_err, X_err, Y2_err, Y2_err, "~>");
+ 
   [a, b] = plotInterval(X);
 
   title(tit);
@@ -21,4 +29,6 @@ function plot2yy(X, Y1, Y2, X_err, Y1_err, Y2_err, tit, xl, yl1, yl2, leg1, leg2
   ylabel(ax(1), yl1);
   ylabel(ax(2), yl2);
   legend(leg1, leg2);
+  set(h1, "color", color1);
+  set(h2, "color", color2);
 endfunction
